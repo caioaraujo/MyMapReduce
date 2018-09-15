2074d9d4d2fb3593e27c88b06c4c1c79e08512f2
caioaraujo <c.felipe.araujo@gmail.com>
2018-09-15 15:27:57 -0300

    hello world

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // Dado um arquivo como entrada, interpreta o mesmo
+        // para processar suas linhas e gerar as entradas
+
+       // TODO
+    }
+}
