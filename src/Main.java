1505d836ba34f30342695d5159d61d02c5dc9acc
caioaraujo <c.felipe.araujo@gmail.com>
2018-09-18 19:26:52 -0300

    aaa

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    public static class TokenizerMapper
+    public static class CompromiseMapper
             extends Mapper<Object, Text, Text, IntWritable> {
 
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
+            /* StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
                 word.set(itr.nextToken());
                 context.write(word, one);
             }
+            */
         }
     }
 
